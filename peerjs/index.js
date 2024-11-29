048eaf895c0927a7568f330100de019c41d646bb
visin04 <vishnuvr22001@gmail.com>
2024-11-30 00:31:19 +0530

    Your commit message here

--- peerjs/index.js
+++ peerjs/index.js
@@ -1,5 +1,6 @@
 const { PeerServer } = require("peer");
 
-const peerServer = PeerServer({ port: 9000, path: "/" });
 
-console.log("PeerServer is running on port 9000");
+PeerServer({ port: 9000, path: "/" });
+
+console.log("server running at 9000")
